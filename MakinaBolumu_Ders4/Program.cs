14625207e29e860cf771ad7bd2b94bc394e638ae
şenol demirci <senol@dpu.edu.tr>
2024-03-08 11:10:29 +0300

    ders4

---
+++ MakinaBolumu_Ders4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakinaBolumu_Ders4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // klavyeden öğrenci no, ad soyad, vize ve final notlarını alıp ortalamayı hesaplar.
+            Console.Write("Öğrenci No Giriniz");
+            string ogrno = Console.ReadLine();
+
+            Console.Write("Ad Soyad Giriniz");
+            string adSoyad = Console.ReadLine();
+
+            Console.Write("Vize Notu : ");
+            float vize = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Final Notu : ");
+            float final = Convert.ToSingle(Console.ReadLine());
+
+            float ortalama = vize * 0.40f + final * 0.60f;
+            Console.WriteLine("Sınav Ortalaması : " + ortalama);
+            Console.WriteLine("Sınav Ortalaması {0}",ortalama);
+
+
+
+        }
+    }
+}
